3800324d8f9fa43285dc23c882678d70114888fa
avagraced <63106462+avagraced@users.noreply.github.com>
2020-04-03 14:17:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let name = "Ava";
+let age = 14;
+let school = "CLV";
+
+console.log("Hi, my name is " + name); 
+
+console.log(" I am " + age + " years old");
+console.log("I attend " + school);
+
+let numberOne;
+let  numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+
+total = numberOne + numberTwo;
+console.log(total);
+};
+
+addition(20, 30);
+addition(30, 70);
+addition(40, 15);
+addition(400, 150);
+
+
